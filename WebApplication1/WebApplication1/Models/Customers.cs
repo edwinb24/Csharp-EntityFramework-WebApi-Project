f014c2ed344a8553749ed59c51e67da775501681
edwinb24 <ebroce@bhhsselectstl.com>
2015-12-28 15:24:11 -0600

    Before Delete of ValuesController
    At this point within the Project inside the Controllers folder there's a
    ValuesController. We don't need this control as it just contains an
    example of a Web API controller.
    
    Delete the ValuesController class and Build the project.

---
+++ WebApplication1/WebApplication1/Models/Customers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Customers
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public int Price { get; set; }
+
+        //Navigation Property
+        public ICollection<orders> Orders { get; set; }
+    }
+</review>
+}
---
+++ WebApplication1/WebApplication1/Models/Orders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Order
+    {
+        public int ReviewId { get; set; }
+
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        //Navigation Property
+        public Customers Customers { get; set; }
+    }
+}
